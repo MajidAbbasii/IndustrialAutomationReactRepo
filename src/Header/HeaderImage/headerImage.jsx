5f1a73fc870a7d05dc022b4a82399d834ba8bb58
Majid Abbasi <m.abbasi201@gmail.com>
2023-08-25 04:22:00 +0430

    EditHeaderComponents

--- src/Header/HeaderImage/headerImage.jsx
+++ src/Header/HeaderImage/headerImage.jsx
@@ -4,7 +4,7 @@ const HeaderImage = () =>{
     return (
         <div class="logo-header mostion">
             <a href="index.html">
-                <img src="images/logo.png" width="193" height="89" alt=""/>
+                <img src="../assets/images/logo.png" width="193" height="89" alt=""/>
             </a> 
         </div>
     );
--- src/Header/HeaderTopBar/headerTopBar.jsx
+++ src/Header/HeaderTopBar/headerTopBar.jsx
@@ -10,14 +10,10 @@ const HeaderTopBar = () =>{
                         <ul class="social-bx list-inline pull-right">
                             <li><a href="javascript:void(0);" class="fa fa-facebook"></a></li>
                             <li><a href="javascript:void(0);" class="fa fa-twitter"></a></li>
-                            <li><a href="javascript:void(0);" class="fa fa-linkedin"></a></li>
-                            <li><a href="javascript:void(0);" class="fa fa-facebook"></a></li>
-                            <li><a href="javascript:void(0);" class="fa fa-twitter"></a></li>
-                            <li><a href="javascript:void(0);" class="fa fa-linkedin"></a></li>
-                        </ul>
-                    </div>
+                            <li><a href="javascript:void(0);" class="fa fa-linkedin"></a></li></ul>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
